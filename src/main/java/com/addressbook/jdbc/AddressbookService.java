69952dfb0b710da237f2af80ec6192e849397181
7506574455 <sanketlalge09@gmail.com>
2021-08-02 07:54:06 -0700

    Added: Retrieve contacts database program

--- src/main/java/com/addressbook/jdbc/AddressbookService.java
+++ src/main/java/com/addressbook/jdbc/AddressbookService.java
@@ -1,5 +1,6 @@
 package com.addressbook.jdbc;
 
+import java.time.LocalDate;
 import java.util.*;
 import com.addressbook.jdbc.Person;
 
@@ -19,12 +20,39 @@ public class AddressbookService {
         this.personList = personList;
     }
     
-    
-     //checking address book of data equal 
+    // purpose:checking address book of data equal 
      
     public List<Person> readAddressBookData(IOService ioService) {
         if (ioService.equals(IOService.DB_IO))
             this.personList = addressBookDBService.readData();
         return personList;
+    }
+    
+    public void updateContactNumber(String firstName, String phoneNo) {
+        int result = addressBookDBService.updateContactNumber(firstName, phoneNo);
+        if (result == 0) return;
+        Person person = this.getAddressBookData(firstName);
+        if (person != null)
+            person.phoneNo = phoneNo;
+
+
+    }
+
+    public Person getAddressBookData(String firstName) {
+        return this.personList.stream()
+                .filter(addressBookDataItem -> addressBookDataItem.getFirstName().equals(firstName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean checkAddressBookInSyncWithDB(String firstName) {
+        List<Person> personList = addressBookDBService.getaddressBookData(firstName);
+        return personList.get(0).equals(getAddressBookData(firstName));
+    }
+
+    public List<Person> readAddressBookForDateRange(IOService ioService, LocalDate startDate, LocalDate endDate) {
+        if (ioService.equals(IOService.DB_IO))
+            return addressBookDBService.getAddressBookForDateRange(startDate, endDate);
+        return null;
     }
 }
--- src/test/java/com/addressbook/jdbc/AddressBookServiceTest.java
+++ src/test/java/com/addressbook/jdbc/AddressBookServiceTest.java
@@ -3,12 +3,11 @@ package com.addressbook.jdbc;
 import com.addressbook.jdbc.AddressbookService;
 import com.addressbook.jdbc.Person;
 
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
+import java.time.LocalDate;
 import java.util.List;
 
 public class AddressBookServiceTest {
@@ -16,11 +15,31 @@ public class AddressBookServiceTest {
     public void init() {
         System.out.println("Welcome to AddressBook Service Program");
     }
-
     @Test
     public void givenAddressBookDB_WhenRetrivedShouldMatchPersonCount() {
-        AddressbookService addressbookService = new AddressbookService();
-        List<Person> addressBookData = addressbookService.readAddressBookData(AddressbookService.IOService.DB_IO);
+        AddressbookService addressBookService = new AddressbookService();
+        List<Person> addressBookData = addressBookService.readAddressBookData(AddressbookService.IOService.DB_IO);
         Assert.assertEquals(2, addressBookData.size());
     }
-}
+    
+   
+    @Test
+    public void givenContactNumber_WhenUpdated_ShouldSyncWithDb() {
+        AddressbookService addressBookService = new AddressbookService();
+     addressBookService.updateContactNumber("sanket","11111111");
+        boolean result = addressBookService.checkAddressBookInSyncWithDB("sanket");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenDateRangeWhenRetrieved_ShouldMatchEntryCount() {
+        AddressbookService addressBookService = new AddressbookService();
+        LocalDate startDate = LocalDate.of(2017, 01, 01);
+        LocalDate endDate = LocalDate.now();
+        List<Person> addressBookDataList =
+                addressBookService.readAddressBookForDateRange(AddressbookService
+                        .IOService.DB_IO, startDate, endDate);
+        Assert.assertEquals(2, addressBookDataList.size());
+    }  
+    }  
+
